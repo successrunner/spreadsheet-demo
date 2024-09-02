1459e9340eae8e378457c48caaf58bac41a65037
successrunner <successrunner0222@gmail.com>
2024-09-02 14:30:26 -0400

    add simple-import-sort

--- eslint.config.js
+++ eslint.config.js
@@ -5,14 +5,15 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import eslintPluginPrettier from 'eslint-plugin-prettier';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
     extends: [
+      eslintConfigPrettier,
       js.configs.recommended,
       ...tseslint.configs.recommended,
-      eslintConfigPrettier,
     ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
@@ -23,13 +24,16 @@ export default tseslint.config(
       prettier: eslintPluginPrettier,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'simple-import-sort': simpleImportSort,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      ...reactHooks.configs.recommended.rules,
     },
   }
 );
